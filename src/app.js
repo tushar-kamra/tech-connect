3c67517c1874df212f77dc8af94b1a34437cb91a
tushar-kamra <tushar.kamra25@gmail.com>
2024-10-21 21:34:00 +0530

    request handler

--- src/app.js
+++ src/app.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const port = 2000;
 
+// this will match all the HTTP method API calls to /test/*
 app.use("/test", (req, res) => {
     res.send("test page");
 });
 
-app.use("/hello", (req, res) => {
-    res.send("hello page");
-});
+// app.use("/hello", (req, res) => {
+//     res.send("hello page");
+// });
 
-app.use("/", (req, res) => {
-    res.send("Homepage");
+// app.use("/", (req, res) => {
+//     res.send("Homepage");
+// });
+
+// This will handle only GET requests to /user
+app.get("/user", (req, res) => {
+    res.send({ fname: "tushar", lname: "kamra" });
 });
 
 app.listen(port, () => {
