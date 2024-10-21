d81f9754c743131d70d894d0adbdb3ec3adf2b97
tushar-kamra <tushar.kamra25@gmail.com>
2024-10-21 21:14:13 +0530

    created express server

--- src/app.js
+++ src/app.js
@@ -2,21 +2,17 @@ const express = require("express");
 const app = express();
 const port = 2000;
 
-
-
-// app.use((req, res, next) => {
-//     res.send("Hello from the servers");
-//     next();
-// });
-
-app.use("/", (req, res) => {
-    res.send("Homepage");
-});
-
 app.use("/test", (req, res) => {
     res.send("test page");
 });
 
+app.use("/hello", (req, res) => {
+    res.send("hello page");
+});
+
+app.use("/", (req, res) => {
+    res.send("Homepage");
+});
 
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`);
