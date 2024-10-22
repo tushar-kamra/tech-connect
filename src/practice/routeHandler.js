8a1653eb0eedcfdfa6278f255691ca25a4650942
tushar-kamra <tushar.kamra25@gmail.com>
2024-10-22 19:38:30 +0530

    changes

---
+++ src/practice/routeHandler.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+
+// this will match all the HTTP method API calls to /test/*
+app.use("/test", (req, res) => {
+    res.send("test page");
+});
+
+// app.use("/hello", (req, res) => {
+//     res.send("hello page");
+// });
+
+// app.use("/", (req, res) => {
+//     res.send("Homepage");
+// });
+
+// app.use("/user", (req, res, next) => {
+//     // res.send("user page");
+//     next();
+// });
+
+// This will handle only GET requests to /user
+app.get("/user", (req, res) => {
+    console.log(req.query);
+    res.send({ fname: "tushar", lname: "kamra" });
+});
+
+// This will handle only POST requests to /user
+app.post("/user", (req, res) => {
+    res.send("POST req successful");
+});
+
+// dynamic routes
+app.get("/user/:userId", (req, res) => {
+    console.log(req.params);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening to port ${port}`);
+});
