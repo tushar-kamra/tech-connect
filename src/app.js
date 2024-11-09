e098281c13d7ec7fd6af7f95ead1f8d69015cb3d
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-09 21:17:59 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const { adminAuth, userAuth } = require("./middlewares/auth");
 
 const app = express();
 const port = 2000;
+
+app.use("/user", userAuth, (req, res) => {
+    res.send("User data sent");
+});
+
+app.use("/admin", adminAuth);
+
+app.use("/admin/deleteUser", (req, res) => {
+    res.send("User deleted");
+});
+
+app.use("/admin/addUser", (req, res) => {
+    res.send("User added");
+});
 
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`);
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,24 @@
-
-
 const adminAuth = (req, res, next) => {
-    
-}
+    const token = "xyz";
+    const isAdminAuthorized = token === "xyz";
+    if (isAdminAuthorized) {
+        next();
+    } else {
+        res.status(401).send("Unauthorized request");
+    }
+};
+
+const userAuth = (req, res, next) => {
+    const token = "xyz";
+    const isUserAuthorized = token === "xyz";
+    if (isUserAuthorized) {
+        next();
+    } else {
+        res.status(401).send("Unauthorized request");
+    }
+};
+
+module.exports = {
+    adminAuth,
+    userAuth,
+};
