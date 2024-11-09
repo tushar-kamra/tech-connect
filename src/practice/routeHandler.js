12229ccfb3c5c190d000231b77714bf167e4cfe5
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-09 20:06:15 +0530

    route handler practice

--- src/practice/routeHandler.js
+++ src/practice/routeHandler.js
@@ -36,6 +36,20 @@ app.get("/user/:userId", (req, res) => {
     console.log(req.params);
 });
 
+app.use(
+    "/user",
+    (req, res, next) => {
+        next();
+        console.log("route handler 1");
+        // res.send("response 1");
+    },
+    (req, res, next) => {
+        console.log("route handler 2");
+        // res.send("response 2");
+        next();
+    }
+);
+
 app.listen(port, () => {
     console.log(`Server listening to port ${port}`);
 });
