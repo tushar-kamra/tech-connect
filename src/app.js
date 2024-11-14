dd54f1e8fdec20d1945ee6b072fcc12913b1d73c
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-14 21:44:53 +0530

    api created

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/database");
+const User = require("./models/user");
 const app = express();
 const port = 2000;
 
@@ -8,4 +9,23 @@ connectDB().then(() => {
     app.listen(port, () => {
         console.log("server connected");
     });
+});
+
+app.post("/signup", async (req, res) => {
+    const user = new User({
+        firstName: "Shivam",
+        lastName: "Kamra",
+        email: "shivam@techconnect.com",
+        password: 123456,
+        age: 27,
+        city: "Delhi",
+        gender: "Male",
+    });
+
+    try {
+        await user.save();
+        res.send("User created successfully!");
+    } catch (err) {
+        res.status(400).send("Error creating user: " + err.message);
+    }
 });
