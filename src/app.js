068729e38d0f7ca86661e25a31e3b90a4b395523
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-21 20:46:40 +0530

    auth express router added

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
 const { validateSignUp } = require("./utils/validation");
-const validator = require("validator");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { userAuth } = require("./middlewares/auth");
+const authRouter = require("./routes/auth");
 const app = express();
 const port = 2000;
 
@@ -19,27 +19,7 @@ connectDB().then(() => {
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.post("/signup", express.json(), async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = new User({
-        firstName,
-        lastName,
-        email,
-        password: passwordHash,
-    });
-
-    try {
-        validateSignUp(req);
-        await user.save();
-        res.send("User created successfully!");
-    } catch (err) {
-        res.status(400).send("Error creating user: " + err.message);
-    }
-});
+app.use("/", authRouter);
 
 // get user by email
 app.get("/user", async (req, res) => {
@@ -107,32 +87,6 @@ app.patch("/user/:userId", async (req, res) => {
         });
         console.log(user);
         res.send("User updated");
-    } catch (err) {
-        res.status(400).send("Something went wrong: " + err.message);
-    }
-});
-
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        if (!validator.isEmail(email)) {
-            throw new Error("Invalid email");
-        }
-
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw new Error("Invalid user");
-        }
-
-        const isPasswordCorrect = await user.validatePassword(password);
-        if (isPasswordCorrect) {
-            const token = await user.getJWT();
-            res.cookie("jwt-token", token);
-            res.send("Login successful");
-        } else {
-            res.send("Invalid credentials");
-        }
     } catch (err) {
         res.status(400).send("Something went wrong: " + err.message);
     }
---
+++ src/routes/auth.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const { validateSignUp } = require("../utils/validation");
+const authRouter = express.Router();
+
+authRouter.post("/signup", async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = new User({
+        firstName,
+        lastName: lastName,
+        email,
+        password: passwordHash,
+    });
+
+    try {
+        validateSignUp(req);
+        await user.save();
+        res.send("User created successfully!");
+    } catch (err) {
+        res.status(400).send("Error creating user: " + err.message);
+    }
+});
+
+authRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        if (!validator.isEmail(email)) {
+            throw new Error("Invalid email");
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            throw new Error("Invalid user");
+        }
+
+        const isPasswordCorrect = await user.validatePassword(password);
+        if (isPasswordCorrect) {
+            const token = await user.getJWT();
+            res.cookie("jwt-token", token);
+            res.send("Login successful");
+        } else {
+            res.send("Invalid credentials");
+        }
+    } catch (err) {
+        res.status(400).send("Something went wrong: " + err.message);
+    }
+});
+
+module.exports = authRouter;
