7283ac2c6c584f687d356c5fcab16a8903c88885
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-23 20:32:33 +0530

    send request api - 1

--- src/routes/request.js
+++ src/routes/request.js
@@ -7,6 +7,14 @@ requestRouter.post(
     userAuth,
     async (req, res) => {
         try {
+            const fromUserId = req.user._id;
+            const status = req.params.status;
+            const toUserId = req.params.toUserId;
+
+            const allowedStatus = ["ignored", "interested"];
+            if (!allowedStatus.includes(status)) {
+                return res.json({ message: "Invalid status" });
+            }
         } catch (err) {
             res.status(400).send("Something went wrong: " + err.message);
         }
