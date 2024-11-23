5f0f6a24acfa38d121d589d6e330e23c13f0aa07
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-23 20:30:24 +0530

    dummy

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,1 +1,14 @@
-const express = require('express');
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const requestRouter = express.Router();
+
+requestRouter.post(
+    "/request/send/:status/:toUserId",
+    userAuth,
+    async (req, res) => {
+        try {
+        } catch (err) {
+            res.status(400).send("Something went wrong: " + err.message);
+        }
+    }
+);
