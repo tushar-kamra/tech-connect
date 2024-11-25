f73efcc5969e8ac3a1fc95710674927b70c8a283
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-25 21:01:39 +0530

    get connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -10,7 +10,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUserId,
             status: "interested",
-        }).populate("fromUserId", ["firstName", "lastName", "about"]);
+        }).populate("fromUserId", ["firstName", "lastName", "about", "skills"]);
 
         res.json({
             message: "Data fetched successfully",
@@ -19,6 +19,30 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     } catch (err) {
         res.status(400).send("Something went wrong: " + err.message);
     }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    const loggedInUserId = req.user._id;
+
+    const connectionRequests = await ConnectionRequest.find({
+        $or: [
+            { fromUserId: loggedInUserId, status: "accepted" },
+            { toUserId: loggedInUserId, status: "accepted" },
+        ],
+    })
+        .populate("fromUserId", ["firstName", "lastName", "about", "skills"])
+        .populate("toUserId", ["firstName", "lastName", "about", "skills"]);
+
+    const data = connectionRequests.map((row) => {
+        // if (row.fromUserId._id.equals(loggedInUserId)) {
+        if (row.fromUserId._id.toString() === loggedInUserId.toString()) {
+            return row.toUserId;
+        } else {
+            return row.fromUserId;
+        }
+    });
+
+    res.json({ data });
 });
 
 module.exports = userRouter;
