f27bbc104301095fae2dc11b91c11e95cd98dd5e
tushar-kamra <tushar.kamra25@gmail.com>
2024-11-25 20:30:55 +0530

    request review api added + ref/populate

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,24 @@
-const express = require('express');
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
 const userRouter = express.Router();
 
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
 
+        const connectionRequests = await ConnectionRequest.find({
+            toUserId: loggedInUserId,
+            status: "interested",
+        }).populate("fromUserId", ["firstName", "lastName", "about"]);
+
+        res.json({
+            message: "Data fetched successfully",
+            data: connectionRequests,
+        });
+    } catch (err) {
+        res.status(400).send("Something went wrong: " + err.message);
+    }
+});
 
 module.exports = userRouter;
